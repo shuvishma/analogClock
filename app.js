72cabec7803a232d7dc6bd02db35c46b38d69592
shuvishma <36675563+shuvishma@users.noreply.github.com>
2021-08-16 00:44:32 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+let hourHand = document.querySelector(".hand-hour");
+let minuteHand = document.querySelector(".hand-minute");
+let secondHand = document.querySelector(".hand-second");
+
+function getTime() {
+  let now = new Date();
+  //console.log(now);
+  let seconds = now.getSeconds();
+  let secondDegree = (seconds / 60) * 360 + (-90) ;
+  secondHand.style.transform = `rotate(${secondDegree}deg)`;
+
+  const minutes = now.getMinutes();
+  const minutesDegree = (minutes / 60) * 360 + (-90) ;
+  minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegree = (hours / 60) * 360 + (-90);
+  hourHand.style.transform = `rotate(${hoursDegree}deg)`;
+}
+
+setInterval(getTime, 1000);
