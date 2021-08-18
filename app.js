e1dc61b852063547697074ba30ee9f020f9e2beb
shuvishma <36675563+shuvishma@users.noreply.github.com>
2021-08-18 22:03:43 +0530

    saved

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ function getTime() {
   let now = new Date();
   //console.log(now);
   let seconds = now.getSeconds();
-  let secondDegree = (seconds / 60) * 360 + (-90) ;
+  let secondDegree = (seconds / 60) * 360 + 90 ;
   secondHand.style.transform = `rotate(${secondDegree}deg)`;
 
   const minutes = now.getMinutes();
-  const minutesDegree = (minutes / 60) * 360 + (-90) ;
+  const minutesDegree = ((minutes / 60) * 360) + ((seconds/60)*6) - 90 ;
   minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
 
   const hours = now.getHours();
-  const hoursDegree = (hours / 60) * 360 - 140;
+  const hoursDegree = ((hours / 12) * 360) + ((minutes/60)*30) - 90;
   hourHand.style.transform = `rotate(${hoursDegree}deg)`;
 }
 
